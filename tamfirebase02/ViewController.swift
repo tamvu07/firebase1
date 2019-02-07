7fe085851c7cdac713577e6b8c6e91744e7e90cc
mac <mac@macs-iMac.local>
2019-02-07 03:01:29 -0800

    Initial Commit

---
+++ tamfirebase02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tamfirebase02
+//
+//  Created by mac on 2/7/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
